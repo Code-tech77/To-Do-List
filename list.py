c1d1de645867dc0f6fa2947c97057e31f92cd374
Mohammed Waleed <119446912+Code-tech77@users.noreply.github.com>
2023-03-08 19:33:29 +0000

    Add files via upload

---
+++ list.py
@@ -0,0 +1,99 @@
+#base
+import tkinter
+from tkinter import *
+
+root = Tk()
+root.title("To-Do-List")
+root.geometry("400x650+400+100")
+root.resizable(False,False)
+
+task_list= []
+
+def addTask():
+     task = task_entry.get()
+     task_entry.delete(0, END)
+
+     if task:
+          with open("tasklist.text", 'a') as taskfile:
+               taskfile.write(f"\n{task}")
+          task_list.append(task)
+          listbox.insert( END, task)
+
+def deleteTask():
+     global task_list
+     task = str(listbox.get(ANCHOR))
+     if task in task_list:
+          task_list.remove(task)
+          with open("tasklist.txt",'w') as taskfie:
+               for task in task_list:
+                    taskfie.write(task+"\n")
+
+          listbox.delete( ANCHOR)
+
+def openTaskFile():
+    try:
+        global task_list
+        with open("tasklist.txt","r") as taskfile:
+            tasks = taskfile.readline()
+
+        for task in tasks:
+                if task !='\n':
+                   task_list.append(task)
+                   listbox.insert(END , task)
+
+    except:
+         file=open('tasklist.txt','w')
+         file.close()
+
+#icon section
+Image_icon=PhotoImage(file="Images/task2.png")
+root.iconphoto(False,Image_icon)
+
+#top bar section
+TopImage=PhotoImage(file="Images/topbar.png")
+Label(root,image=TopImage).pack()
+
+dockImage=PhotoImage(file="Images/dock.png")
+Label(root,image=dockImage,bg="#32405b").place(x=30,y=25)
+
+noteImage=PhotoImage(file="Images/task2.png")
+Label(root,image=noteImage,bg="#32405b").place(x=340,y=25)
+
+heading=Label(root,text="Mohammed Waleed", font="arial 15 bold",fg="white",bg="#32405b")
+heading.place(x=130,y=20)
+
+#main section
+frame= Frame(root,width=400,height=50,bg="white")
+frame.place(x=0,y=180)
+
+task=StringVar()
+task_entry=Entry(frame,width=18, font="arial 20",bd=0)
+task_entry.place(x=10,y=7)
+task_entry.focus()
+
+#add button section
+button=Button(frame,text="ADD",font="arial 20 bold",width=6, bg="#5a95ff",fg="#fff",bd=0,command=addTask)
+button.place(x=300,y=0)
+
+#listbox section
+frame1=Frame(root,bd=3,width=700,height=280,bg="#32405b")
+frame1.pack(pady=(160,0))
+
+listbox= Listbox(frame1,font=("arial", 12),width=40,height=16,bg="#32405b", fg="white", cursor="hand2", selectbackground="#5a95ff")
+listbox.pack(side=LEFT , fill=BOTH, padx=2)
+
+#scrollbar
+Scrollbar= Scrollbar(frame1)
+Scrollbar.pack(side=RIGHT, fill=BOTH)
+
+listbox.config(yscrollcommand=Scrollbar.set)
+Scrollbar.config(command=listbox.yview)
+
+openTaskFile()
+
+#delete function section
+Delete_icon=PhotoImage(file="Images/delete.png")
+Button(root,image=Delete_icon,bd=0, command=deleteTask).pack(side=BOTTOM,pady=13)
+
+
+root.mainloop()
